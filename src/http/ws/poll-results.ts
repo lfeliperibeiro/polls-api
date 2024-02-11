2c913dfe50f7086b34902e3b9ca36b6fea3a4e76
Felipe Ribeiro <piporibeiro@gmail.com>
2024-02-10 23:59:16 -0300

    feat: add pollResults WebSocket endpoint

---
+++ src/http/ws/poll-results.ts
@@ -0,0 +1,23 @@
+import { FastifyInstance } from 'fastify'
+import { voting } from '../../utils/voting-pub-sub'
+import z from 'zod'
+
+export async function pollResults(app: FastifyInstance) {
+  app.get(
+    '/polls/:pollId/results',
+    { websocket: true },
+    (connection, request) => {
+      connection.socket.on('message', (message: string) => {
+        // inscrever apenas nas mensagens publicadas no canal com o id da enquete('pollId')
+        const getPollParams = z.object({
+          pollId: z.string().uuid(),
+        })
+
+        const { pollId } = getPollParams.parse(request.params)
+        voting.subscribe(pollId, (message) => {
+          connection.socket.send(JSON.stringify(message))
+        })
+      })
+    },
+  )
+}
